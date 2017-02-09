c50d3a808e4a57b36ae53075c499e5abe2693622
VasylKosowski <vasyl.kosowski@gmail.com>
2017-02-09 11:19:24 +0100

    - base files added;

---
+++ src/App.js
@@ -0,0 +1,12 @@
+/**
+ * Created by SG0226637 on 2/9/2017.
+ */
+import React, { Component } from 'react';
+
+export default class App extends Component {
+    render() {
+        return (
+            <h1>AutoComplete From GitHub</h1>
+        );
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by SG0226637 on 2/9/2017.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
